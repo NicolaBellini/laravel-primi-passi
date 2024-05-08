740c5ef4b4c2b2a0b1d696a9a819f8117e2f0faf
NicolaBellini <bellininicola4366@gmail.com>
2024-05-08 14:58:34 +0200

    [included] css in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,33 @@
 @include('layouts.header')
 
 
-<div class="container">
-    <h1>HOME cisao</h1>
+<div class="jumbotron jumbotron-fluid bg-dark text-white">
+    <div class="container">
+        <h1 class="display-4">Nicola Bellini</h1>
+        <p class="lead">Prima esercitazione con Laravel</p>
+        <hr class="my-4">
+
+        <a class="btn btn-primary btn-lg mb-2 " href="{{ route('about') }}" role="button">Scopri di più</a>
+    </div>
 </div>
+
+
+<div class="gallery">
+    <!-- Immagine 1 -->
+    <div class="item">
+        <img src="image1.jpg" alt="Immagine 1">
+    </div>
+    <!-- Immagine 2 -->
+    <div class="item">
+        <img src="image2.jpg" alt="Immagine 2">
+    </div>
+    <!-- Aggiungi altre immagini qui -->
+    <!-- Immagine 20 -->
+    <div class="item">
+        <img src="image20.jpg" alt="Immagine 20">
+    </div>
+</div>
+
 
 
 @include('layouts.footer')
